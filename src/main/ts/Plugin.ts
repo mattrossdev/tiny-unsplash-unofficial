2583878c80a50f8890336aca4bcbc481abd9a7d3
Matthew Ross <mcross98@gmail.com>
2024-03-08 16:16:47 -0800

    Add search to displayed image modal

--- src/main/ts/Plugin.ts
+++ src/main/ts/Plugin.ts
@@ -8,13 +8,16 @@ const setup = (editor: Editor, url: string): void => {
   const unsplash_api_key = tinymce.activeEditor.options.get("unsplash_api_key");
 
   const handleImageClick = (src) => {
+    console.log("going to close");
     tinymce.activeEditor.windowManager.close();
+    console.log("should be closed now");
     tinymce.activeEditor.execCommand("mceImage");
+    console.log("image uploader should be here");
 
     /* The modal field is not immediately visible. 
   It will fail to find the element without a timeout (hacky solution) */
     setTimeout(function () {
-      //This is subject to break
+      //This is subject to change/not an ideal selector
       //TODO: Find a better way of targeting this
       const urlInput: any = document.querySelector(
         'input[type="url"].tox-textfield'
@@ -28,21 +31,26 @@ const setup = (editor: Editor, url: string): void => {
       }
     }, 50);
   };
-  document.addEventListener("click", (event: any) => {
+
+  /* Click event listener doesn't work. 
+  If you use click and are focused into a text field that's out of view,
+  the callback doesn't happen */
+  document.addEventListener("mousedown", (event: any) => {
+    console.log("click");
+    console.log(event.target.classList);
     if (event.target.classList.contains("unsplash-search-img")) {
+      console.log("should respond to click");
       handleImageClick(event.target.src);
     }
   });
 
   const openImageDialog = (imageData) => {
-    console.log(imageData.length);
     const generateImageHTML = (image) => {
       const src = image.urls.small;
       const user = image.user.username;
       const profile = image.user.links.html;
-      console.log("here");
       let imageHTML = `<div style="display: flex; flex-direction: column; align-items: center;">`;
-      imageHTML += `<span> <img src="${src}" class=unsplash-search-img style = "max-width: 80%;">`;
+      imageHTML += `<span> <img src="${src}" class=unsplash-search-img style = "max-width: 80%; cursor: pointer;">`;
       imageHTML += `<span style = "margin-top: 5px"> Photo by <a href = ${profile}>@${user} </a> </span>`;
       imageHTML += `</div>`;
       return imageHTML;
@@ -56,18 +64,32 @@ const setup = (editor: Editor, url: string): void => {
         imageGridHTML += generateImageHTML(imageData[i]);
       }
       imageGridHTML += "</div>";
-      console.log(imageGridHTML);
       return imageGridHTML;
     };
 
     const imageGridHTML = generateImageGridHTML();
-    console.log(imageGridHTML);
 
     return editor.windowManager.open({
       title: "Results via Unsplash",
       body: {
         type: "panel",
         items: [
+          {
+            type: "input",
+            name: "search",
+            label: "Search",
+          },
+          {
+            type: "selectbox",
+            name: "orientation",
+            label: "Orientation",
+            size: 1,
+            items: [
+              { value: "all", text: "All" },
+              { value: "landscape", text: "Landscape" },
+              { value: "portrait", text: "Portrait" },
+            ],
+          },
           {
             type: "htmlpanel",
             html: imageGridHTML,
@@ -86,9 +108,15 @@ const setup = (editor: Editor, url: string): void => {
         },
       ],
       onSubmit: (api) => {
-        tinymce.activeEditor.execCommand("mceImage");
-        /* Insert content when the window form is submitted */
-        //editor.insertContent("Title: " + data.title);
+        const search = api.getData().search;
+        const orientation = api.getData().orientation;
+        api.close();
+        fetchImages(search, orientation, unsplash_api_key)
+          .then((imageData) => {
+            api.close();
+            openImageDialog(imageData);
+          })
+          .catch((error) => {});
       },
     });
   };
